94b24e760196c4ca8e4a4a1092ca1c2d7c0e08f8
Amit Kittur <amit.kittur@gmail.com>
2019-09-19 11:47:47 +0530

    Initial Commit

---
+++ DattingApp.API/Data/DataContext.cs
@@ -0,0 +1,11 @@
+using DattingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DattingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options){}
+        public DbSet<Value> Values { get; set; }
+    }
+}
